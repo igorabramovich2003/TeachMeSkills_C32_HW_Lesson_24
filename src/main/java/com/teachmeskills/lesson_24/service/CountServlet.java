e1a548c8be8ba38997f4503d84435649397efa0e
Igor Abramovich <igorab2033@gmail.com>
2025-01-07 13:27:55 +0300

    init commit2

--- src/main/java/com/teachmeskills/lesson_24/service/CountServlet.java
+++ src/main/java/com/teachmeskills/lesson_24/service/CountServlet.java
@@ -1,7 +1,6 @@
 package com.teachmeskills.lesson_24.service;
 
 import com.teachmeskills.lesson_24.servlet.Logger;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
